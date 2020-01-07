d6ff180ed1563202185f0ff9cb5909b427e26930
creoludifico <omoeltoft@gmail.com>
2020-01-07 13:58:44 +0100

    Kort og bruger modeller

--- src/main/java/Matador/ChanceCard/CashOutDependentBuildingCard.java
+++ src/main/java/Matador/ChanceCard/CashOutDependentBuildingCard.java
@@ -1,4 +1,19 @@
 package Matador.ChanceCard;
 
 public class CashOutDependentBuildingCard extends ChanceCard {
+    private int housePrice, hotelPrice;
+
+    public CashOutDependentBuildingCard(String name, int housePrice, int hotelPrice) {
+        super(name);
+        this.housePrice = housePrice;
+        this.hotelPrice = hotelPrice;
+    }
+
+    public int getHousePrice() {
+        return housePrice;
+    }
+
+    public int getHotelPrice() {
+        return hotelPrice;
+    }
 }
