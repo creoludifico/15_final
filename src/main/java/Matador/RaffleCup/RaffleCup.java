9f351b4203b9da446b3dcbe63d8c71275a868379
creoludifico <omoeltoft@gmail.com>
2020-01-07 15:53:19 +0100

    Die

--- src/main/java/Matador/RaffleCup/RaffleCup.java
+++ src/main/java/Matador/RaffleCup/RaffleCup.java
@@ -1,4 +1,19 @@
 package Matador.RaffleCup;
 
 public class RaffleCup {
+    private Die d1, d2;
+
+    public RaffleCup(){
+        d1 = new Die();
+        d2 = new Die();
+    }
+
+    public void shakeTheRaffleCup(){
+        d1.rollDie();
+        d2.rollDie();
+    }
+
+    public int getValue(){
+        return d1.getDie() + d2.getDie();
+    }
 }
