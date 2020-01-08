632f3cc71e3c4f07d806d40200909239f540c56e
Alfkil Thorbjørn Wennermark <alfkil@gmail.com>
2020-01-08 21:31:38 +0100

    FerryField rent index updated.

--- src/main/java/Matador/Field/FerryField.java
+++ src/main/java/Matador/Field/FerryField.java
@@ -1,11 +1,7 @@
 package Matador.Field;
 
-import Matador.User.Player;
-
-import java.awt.*;
-
 public class FerryField extends OwnableField {
-    private final int rent[] = new int[]{25, 50, 100, 200};
+    private final int rent[] = new int[]{0, 25, 50, 100, 200};
 
     public FerryField(String title, int price, int mortgage) {
         super(title, "Pris: " + price, price, mortgage);
