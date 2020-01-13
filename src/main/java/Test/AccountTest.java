307d638d6968a85f1be11912d7012f4506d450c4
tobias <tobiasobk@gmail.com>
2020-01-13 13:31:09 +0100

    Test klasser for relevante metoder

---
+++ src/main/java/Test/AccountTest.java
@@ -0,0 +1,20 @@
+package Test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+
+    @Test
+    public void modifyBalance() {
+    }
+
+    @Test
+    public void getBalance() {
+    }
+
+    @Test
+    public void getBalanceAsString() {
+    }
+}
---
+++ src/main/java/Test/PlayerTest.java
@@ -0,0 +1,56 @@
+package Test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getAccount() {
+    }
+
+    @Test
+    public void setInJail() {
+    }
+
+    @Test
+    public void isInJail() {
+    }
+
+    @Test
+    public void setHasEscapeJailCard() {
+    }
+
+    @Test
+    public void hasEscapeJailCard() {
+    }
+
+    @Test
+    public void setFieldIndexx() {
+    }
+
+    @Test
+    public void getFieldIndex() {
+    }
+
+    @Test
+    public void isBonusOnNextRaffle() {
+    }
+
+    @Test
+    public void setBonusOnNextRaffle() {
+    }
+
+    @Test
+    public void getJailForRounds() {
+    }
+
+    @Test
+    public void setJailForRounds() {
+    }
+}
