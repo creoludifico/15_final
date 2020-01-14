e37c15666dc18c513b231be9350ebe28e22c720e
Christian Frost <s184140@student.dtu.dk>
2020-01-14 14:19:47 +0100

    PlayerControllerTest

--- src/main/java/Test/PlayerControllerTest.java
+++ src/main/java/Test/PlayerControllerTest.java
@@ -2,7 +2,6 @@ package Test;
 
 import Matador.Controllers.PlayerController;
 import Matador.GUI.InterfaceGUI;
-import Matador.Models.User.Account;
 import Matador.Models.User.Player;
 import org.junit.Test;
 
@@ -25,55 +24,58 @@ public class PlayerControllerTest {
         }
     }
 
-//    @Test
-//    public void setCurrentPlayer() {
-//    }
-//
-//    @Test
-//    public void getPlayers() {
-//
-//    }
-//
-//
-//    @Test
-//    public void getPlayer() {
-//    }
-
     @Test
     public void getPlayerFromName() {
         InterfaceGUI.initGUI();
         PlayerController playerController = new PlayerController();
 
+        for (int i = 0; i < playerController.getPlayers().length; i++) {
+            playerController.setCurrentPlayer(i);
+            String name = playerController.getCurrentPlayer().getName();
+            Player player = playerController.getPlayerFromName(name);
+            assertEquals(playerController.getCurrentPlayer(),player);
+        }
+            }
 
-//        Player player = playerController.getCurrentPlayer();
-//
-//        int i = 0;
-//        for(Player pl : playerController.getPlayers()){
-//            playerController.setCurrentPlayer(i);
-//            assertEquals(pl.getName(), player.getName());
-//            i++;
-//        }
-
-
-    }
-
-    @Test
-    public void getPlayerNames() {
-    }
-
-    @Test
-    public void movePlayerForwardField() {
-    }
-
-    @Test
-    public void movePlayerToField() {
-    }
 
     @Test
     public void modifyBalance() {
+        InterfaceGUI.initGUI();
+        PlayerController playerController = new PlayerController();
+
+        playerController.setCurrentPlayer(0);
+        int beforeBalance = playerController.getPlayer(0).getAccount().getBalance();
+
+        playerController.getPlayer(0).getAccount().modifyBalance( 2, playerController.getCurrentPlayer().getName());
+        assertEquals(beforeBalance + 2, playerController.getCurrentPlayer().getAccount().getBalance());
+
     }
 
-    @Test
-    public void getAssets() {
-    }
+//    @Test
+//    public void getAssets() {
+//        InterfaceGUI.initGUI();
+//        PlayerController playerController = new PlayerController();
+//        FieldController fieldController = new FieldController();
+//        RaffleCupController raffleCupController = new RaffleCupController();
+//
+//
+//        playerController.setCurrentPlayer(0);
+//        Player player = playerController.getCurrentPlayer();
+//
+////        fieldController.fieldAction(player,1, raffleCupController);
+//        OwnableField[] ownableFields = fieldController.getOwnableFields(player);
+//        for(OwnableField ownableField : ownableFields) {
+//            int property = ownableField.getPrice();
+//
+//
+//            playerController.setCurrentPlayer(0);
+//
+//            int assets = playerController.getAssets(player, true, false, false);
+//            int money = playerController.getCurrentPlayer().getAccount().getBalance();
+//
+//            int total = money + property;
+//            assertEquals(assets, total);
+//
+//        }
+//    }
 }
